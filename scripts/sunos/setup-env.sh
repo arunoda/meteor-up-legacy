b7bf53037a17a8d6a77f0bbe83a3cfea3f96bad3
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-06-18 13:43:45 +0530

    added support for sunos

---
+++ scripts/sunos/setup-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install make and other build tool
+sudo pkgin -y install build-essential
+
+sudo mkdir -p /opt/<%= appName %>/
+sudo mkdir -p /opt/<%= appName %>/config
+sudo mkdir -p /opt/<%= appName %>/tmp
+
+sudo chown ${USER} /opt/<%= appName %> -R
+sudo chown ${USER} /etc/init
+sudo chown ${USER} /etc/
+
+sudo npm install -g userdown wait-for-mongo
+
+# Creating a non-privileged user
+sudo useradd mtuser || :
