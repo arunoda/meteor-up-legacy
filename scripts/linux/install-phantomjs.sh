1b0d321b9488e362064589117dd28832eecc8850
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-08-28 12:43:06 +0530

    changed phantomjs download link * moved from googlecode to bitbucket * use version 1.9.7

--- scripts/linux/install-phantomjs.sh
+++ scripts/linux/install-phantomjs.sh
@@ -3,17 +3,18 @@
 # Remove the lock
 set +e
 sudo rm /var/lib/dpkg/lock > /dev/null
-sudo rm /var/cache/apt/archives/lock > /dev/null
+sudo rm /var/cache/apt/archives/lock > /dev/null  
 sudo dpkg --configure -a
 set -e
 
 # Install PhantomJS
 sudo apt-get -y install libfreetype6 libfreetype6-dev fontconfig > /dev/null
 ARCH=`uname -m`
+PHANTOMJS_VERSION=1.9.7
 
 cd /usr/local/share/
-sudo wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-${ARCH}.tar.bz2 > /dev/null
-sudo tar xjf phantomjs-1.9.1-linux-${ARCH}.tar.bz2  > /dev/null
-sudo ln -s -f /usr/local/share/phantomjs-1.9.1-linux-${ARCH}/bin/phantomjs /usr/local/share/phantomjs
-sudo ln -s -f /usr/local/share/phantomjs-1.9.1-linux-${ARCH}/bin/phantomjs /usr/local/bin/phantomjs
-sudo ln -s -f /usr/local/share/phantomjs-1.9.1-linux-${ARCH}/bin/phantomjs /usr/bin/phantomjs
+sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2 > /dev/null
+sudo tar xjf phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2  > /dev/null
+sudo ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/share/phantomjs
+sudo ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/bin/phantomjs
+sudo ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/bin/phantomjs
