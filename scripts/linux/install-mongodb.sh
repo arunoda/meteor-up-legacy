19ec39336817ee36611cc51af19f4a07e3a9fb44
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-09-05 05:14:25 +0530

    updated for version MongoDB version 2.6

--- scripts/linux/install-mongodb.sh
+++ scripts/linux/install-mongodb.sh
@@ -8,10 +8,10 @@ sudo dpkg --configure -a
 set -e
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update -y
-sudo apt-get install mongodb-10gen
+sudo apt-get install mongodb-org mongodb-org-server mongodb-org-shell mongodb-org-tools -y
 
 # Restart mongodb
-sudo stop mongodb || :
-sudo start mongodb
+sudo stop mongod || :
+sudo start mongod
