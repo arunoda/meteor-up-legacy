394c0af2319b47e3ab96dffadd18b928ed33d7d6
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-03-12 17:46:51 +0530

    Add missing sudo

--- templates/linux/deploy.sh
+++ templates/linux/deploy.sh
@@ -31,10 +31,10 @@ gyp_rebuild_inside_node_modules () {
       echo " > $npmModule: npm install due to binary npm modules"
       rm -rf node_modules
       if [ -f binding.gyp ]; then
-        node-gyp rebuild || :
-        npm install
+        sudo node-gyp rebuild || :
+        sudo npm install
       else
-        npm install
+        sudo npm install
       fi
     fi
 
