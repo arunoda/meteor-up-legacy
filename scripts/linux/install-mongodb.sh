581e7613df3d7733445bec0dfff51a3782ead9bb
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-05-19 04:47:19 +0530

    Add support for sudo users (tested on EC2 ubuntu)

--- scripts/linux/install-mongodb.sh
+++ scripts/linux/install-mongodb.sh
@@ -15,6 +15,6 @@ sudo docker run \
   --restart=always \
   --publish=127.0.0.1:27017:27017 \
   --volume=/var/lib/mongodb:/data/db \
-  --volume=/etc/mongodb.conf:/mongodb.conf \
+  --volume=/opt/mongodb/mongodb.conf:/mongodb.conf \
   --name=mongodb \
   mongo mongod -f /mongodb.conf
--- scripts/linux/setup-env.sh
+++ scripts/linux/setup-env.sh
@@ -3,5 +3,9 @@
 sudo mkdir -p /opt/<%= appName %>/
 sudo mkdir -p /opt/<%= appName %>/config
 sudo mkdir -p /opt/<%= appName %>/tmp
+sudo mkdir -p /opt/mongodb
 
 sudo chown ${USER} /opt/<%= appName %> -R
+sudo chown ${USER} /opt/mongodb -R
+
+sudo usermod -a -G docker ${USER}
