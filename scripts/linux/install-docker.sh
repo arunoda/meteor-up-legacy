5beab16582446c738cff95b8d337814261798532
Callum Macdonald <git@callum-macdonald.com>
2015-06-11 18:31:50 +0200

    Do nothing if docker is already installed.

--- scripts/linux/install-docker.sh
+++ scripts/linux/install-docker.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-# Remove the lock
-set +e
-sudo rm /var/lib/dpkg/lock > /dev/null
-sudo rm /var/cache/apt/archives/lock > /dev/null
-sudo dpkg --configure -a
-set -e
-
-# Required to update system
-sudo apt-get update
-
-# Install docker
+# Is docker already installed?
 set +e
 haveDocker=$(docker version | grep "version")
 set -e
 
 if [ ! "$haveDocker" ]; then
+
+  # Remove the lock
+  set +e
+  sudo rm /var/lib/dpkg/lock > /dev/null
+  sudo rm /var/cache/apt/archives/lock > /dev/null
+  sudo dpkg --configure -a
+  set -e
+
+  # Required to update system
+  sudo apt-get update
+
+  # Install docker
   wget -qO- https://get.docker.com/ | sudo sh
 fi
